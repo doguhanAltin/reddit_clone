e9ee8f44ceadec3213f84cccfafbdf3c074b0048
Dogu <altindoguhan@gmail.com>
2023-04-05 18:53:01 +0300

    [Dev] Directory of communities

--- reddit_clone/src/hooks/useCommunityData.tsx
+++ reddit_clone/src/hooks/useCommunityData.tsx
@@ -99,6 +99,7 @@ export const useCommunityData = () => {
       const newSnippet: CommunitySnippet = {
         communityId: communityData.id,
         imageUrl: communityData.imageUrl || "",
+        isModerator: user?.uid === communityData.creatorId,
       };
       batch.set(
         doc(
